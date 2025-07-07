4f0b8094c5001637690dd291e89938aa2539b2b4
Alex Zaslavsky <alex.zaslavsky@zesty.co>
2025-07-07 13:11:34 +0300

    golang better suggestions

--- plugins.lua
+++ plugins.lua
@@ -1050,168 +1050,174 @@ require("lazy").setup(
       end
     },
     { 'hrsh7th/nvim-cmp',
-    config = function()
-      local t = function(str)
-        return vim.api.nvim_replace_termcodes(str, true, true, true)
-      end
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      dependencies = {
+        { "samiulsami/cmp-go-deep", dependencies = { "kkharji/sqlite.lua" } },
+      },
+      config = function()
+        local t = function(str)
+          return vim.api.nvim_replace_termcodes(str, true, true, true)
+        end
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
-      local luasnip = require("luasnip")
-      local cmp = require('cmp')
-      local lspkind = require('lspkind')
-      cmp.setup {
-        view = {
-          entries = { name = "custom", selection_order = 'near_cursor' }
-        },
-        window = {
-          completion = {
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-            col_offset = -3,
-            side_padding = 0,
+        local luasnip = require("luasnip")
+        local cmp = require('cmp')
+        local lspkind = require('lspkind')
+        cmp.setup {
+          view = {
+            entries = { name = "custom", selection_order = 'near_cursor' }
           },
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-          end,
-        },
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            if vim.tbl_contains({ 'path' }, entry.source.name) then
-              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-              if icon then
-                vim_item.kind = icon
-                vim_item.kind_hl_group = hl_group
-                return vim_item
-              end
-            end
-
-            local completion_item = entry:get_completion_item()
-            local highlights_info = require('colorful-menu').cmp_highlights(entry)
-            if highlights_info == nil then
-              vim_item.abbr = completion_item.label
-            else
-              vim_item.abbr_hl_group = highlights_info.highlights
-              vim_item.abbr = highlights_info.text
-            end
-            local kind = lspkind.cmp_format({ mode='symbol_text', max_width=50, with_text=true, show_labelDetail=true })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
-            return vim_item
-          end
-        },
-        completion = {
-          keyword_length = 0
-        },
-        sources = cmp.config.sources({
-          { name = 'luasnip' },
-          { name = 'nvim_lsp' },
-          { name = 'copilot', priority = 1 },
-          { name = 'codeium', priority = 1 },
-          { name = 'buffer' },
-          { name = 'path' },
-        }),
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
-          ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
-          ['<C-n>'] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
-              end
+          window = {
+            completion = {
+              winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+              col_offset = -3,
+              side_padding = 0,
+              border = "rounded"
+            },
+            documentation = cmp.config.window.bordered()
+          },
+          snippet = {
+            expand = function(args)
+              require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             end,
-            i = function(fallback)
+          },
+          formatting = {
+            fields = { "kind", "abbr", "menu" },
+            format = function(entry, vim_item)
+              if vim.tbl_contains({ 'path' }, entry.source.name) then
+                local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+                if icon then
+                  vim_item.kind = icon
+                  vim_item.kind_hl_group = hl_group
+                  return vim_item
+                end
+              end
+
+              local completion_item = entry:get_completion_item()
+              local highlights_info = require('colorful-menu').cmp_highlights(entry)
+              if highlights_info == nil then
+                vim_item.abbr = completion_item.label
+              else
+                vim_item.abbr_hl_group = highlights_info.highlights
+                vim_item.abbr = highlights_info.text
+              end
+              local kind = lspkind.cmp_format({ mode='symbol_text', max_width=50, with_text=true, show_labelDetail=true })(entry, vim_item)
+              local strings = vim.split(kind.kind, "%s", { trimempty = true })
+              kind.kind = " " .. (strings[1] or "") .. " "
+              kind.menu = "    (" .. (strings[2] or "") .. ")"
+              return vim_item
+            end
+          },
+          completion = {
+            keyword_length = 0
+          },
+          sources = cmp.config.sources({
+            { name = 'luasnip' },
+            { name = 'nvim_lsp' },
+            { name = 'copilot', priority = 1 },
+            { name = 'codeium', priority = 1 },
+            { name = 'go_deep', max_item_count=5, min_keyword_length=3, priority = 2 },
+            { name = 'buffer' },
+            { name = 'path' },
+          }),
+          mapping = {
+            ["<Tab>"] = cmp.mapping(function(fallback)
               if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
               else
                 fallback()
               end
-            end
-          }),
-          ['<C-p>'] = cmp.mapping({
-            c = function()
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
               if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
               else
                 fallback()
               end
-            end
-          }),
-          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.complete()
-            else
-              vim.api.nvim_feedkeys(t('<C-Space>'), 'n', true)
-            end
-          end, { 'i', 'c' }),
-          ['<C-e>'] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
-          ['<CR>'] = cmp.mapping({
-            i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
+            end, { "i", "s" }),
+            ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+            ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+            ['<C-n>'] = cmp.mapping({
+              c = function()
+                if cmp.visible() then
+                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+                end
+              end,
+              i = function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  fallback()
+                end
               end
-            end
-          }),
-        },
-      }
-      -- Use buffer source for `/`.
-      cmp.setup.cmdline('/', {
-        completion = { autocomplete = false },
-        sources = {
-          { name = 'buffer', option = { keyword_pattern = [=[[^[:blank:]].*]=] } }
+            }),
+            ['<C-p>'] = cmp.mapping({
+              c = function()
+                if cmp.visible() then
+                  cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+                end
+              end,
+              i = function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  fallback()
+                end
+              end
+            }),
+            ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+            ['<C-Space>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.complete()
+              else
+                vim.api.nvim_feedkeys(t('<C-Space>'), 'n', true)
+              end
+            end, { 'i', 'c' }),
+            ['<C-e>'] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
+            ['<CR>'] = cmp.mapping({
+              i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+              c = function(fallback)
+                if cmp.visible() then
+                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                else
+                  fallback()
+                end
+              end
+            }),
+          },
         }
-      })
-
-      -- Use cmdline & path source for ':'.
-      cmp.setup.cmdline(':', {
-        completion = { autocomplete = false },
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
+        -- Use buffer source for `/`.
+        cmp.setup.cmdline('/', {
+          completion = { autocomplete = false },
+          sources = {
+            { name = 'buffer', option = { keyword_pattern = [=[[^[:blank:]].*]=] } }
+          }
         })
-      })
+
+        -- Use cmdline & path source for ':'.
+        cmp.setup.cmdline(':', {
+          completion = { autocomplete = false },
+          sources = cmp.config.sources({
+            { name = 'path' }
+          }, {
+            { name = 'cmdline' }
+          })
+        })
       end
     },
     { "ThePrimeagen/refactoring.nvim",
